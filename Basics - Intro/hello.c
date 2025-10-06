8efcae66fd594501d5508a2932777f171ab2d03d
Archana Kumari <93970069+archana8teenth@users.noreply.github.com>
2025-10-06 19:38:29 +0530

    Create hello.c

---
+++ Basics - Intro/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>            //tells the compiler to include info about the standard input/output library 
+main()                        //calls other functions to help perform its job; here defined to be function that expects no arguments, ehich is indicated by a empty list ( ).
+{
+ printf("hello, world\n");    //printf is a library funciton that prints outpur, in this case the string of characters between the quotes
+}
+
+
+
+//character string or string constant- a sequence of charactes in double quotes\\
+// \n - notation for the newline character which when printed advances the output to the left margin on the next line
